c445491a4803c4b2a3a1efbc5ea566f14edd0c14
Sanjar17 <sanjarsaidov173@gmail.com>
2023-07-15 17:32:33 +0500

    fonts added

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            SvgPicture.asset('assets/images/logo.svg'),
+            Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(16),
+                  topLeft: Radius.circular(16),
+                ),
+              ),
+              child: const Text(
+                'Fruit Hub',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontFamily: 'Bad Script',
+                  fontWeight: FontWeight.w400,
+                  height: 40,
+                  letterSpacing: -0.48,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }

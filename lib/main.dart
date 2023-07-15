7c5f0454e6980b228aaeebaf8e4fcb2a9def0d92
Sanjar17 <sanjarsaidov173@gmail.com>
2023-07-15 17:20:33 +0500

    initial

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:e_commerce_mobile_app/route.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ecommerce App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      onGenerateRoute: RouteGenerator.generateRoute,
+    );
+  }
+}
---
+++ lib/route.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import 'screens/screens.dart';
+
+class RouteGenerator {
+  static const String splashScreen = '/splashScreen';
+  static const String welcomScreen = '/welcomScreen';
+  static const String authenticationScreen = '/authenticationScreen';
+  static const String homeScreen = '/homeScreen';
+  static const String productScreen = '/productScreen';
+  static const String cartScreen = '/cartScreen';
+  static const String completeScreen = '/completeScreen';
+  static const String trackingScreen = '/trackingScreen';
+
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case splashScreen:
+        return MaterialPageRoute(builder: (_) => const SplashScreen());
+      case welcomScreen:
+        return MaterialPageRoute(builder: (_) => const WelcomScreen());
+      case authenticationScreen:
+        return MaterialPageRoute(builder: (_) => const AuthenticationScreen());
+      case homeScreen:
+        return MaterialPageRoute(builder: (_) => const HomeScreen());
+      case productScreen:
+        return MaterialPageRoute(builder: (_) => const ProductScreen());
+      case cartScreen:
+        return MaterialPageRoute(builder: (_) => const CartScreen());
+      case completeScreen:
+        return MaterialPageRoute(builder: (_) => const CompleteScreen());
+      case trackingScreen:
+        return MaterialPageRoute(builder: (_) => const TrackingScreen());
+      default:
+        return MaterialPageRoute(builder: (_) => const ErrorScreen());
+    }
+  }
+}

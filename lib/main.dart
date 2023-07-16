11827f508200b5a62e076a831bf0abd4903bed22
Sanjar17 <sanjarsaidov173@gmail.com>
2023-07-16 11:47:24 +0500

    animation added to topic

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:e_commerce_mobile_app/providers/index_provider.dart';
 import 'package:e_commerce_mobile_app/route.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const App());
@@ -10,28 +12,31 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Ecommerce App',
-      theme: ThemeData(
-        primarySwatch: const MaterialColor(
-          0xFFFFA451,
-          {
-            50: Color(0xFFFFF3E0),
-            100: Color(0xFFFFE0B2),
-            200: Color(0xFFFFCC80),
-            300: Color(0xFFFFB74D),
-            400: Color(0xFFFFA726),
-            500: Color(0xFFFFA451),
-            600: Color(0xFFFF8F00),
-            700: Color(0xFFFF6F00),
-            800: Color(0xFFFF5722),
-            900: Color(0xFFF4511E),
-          },
+    return ChangeNotifierProvider(
+      create: (context) => IndexProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Ecommerce App',
+        theme: ThemeData(
+          primarySwatch: const MaterialColor(
+            0xFFFFA451,
+            {
+              50: Color(0xFFFFF3E0),
+              100: Color(0xFFFFE0B2),
+              200: Color(0xFFFFCC80),
+              300: Color(0xFFFFB74D),
+              400: Color(0xFFFFA726),
+              500: Color(0xFFFFA451),
+              600: Color(0xFFFF8F00),
+              700: Color(0xFFFF6F00),
+              800: Color(0xFFFF5722),
+              900: Color(0xFFF4511E),
+            },
+          ),
         ),
+        initialRoute: RouteGenerator.splashScreen,
+        onGenerateRoute: RouteGenerator.generateRoute,
       ),
-      initialRoute: RouteGenerator.splashScreen,
-      onGenerateRoute: RouteGenerator.generateRoute,
     );
   }
 }
--- lib/widgets/topic_row.dart
+++ lib/widgets/topic_row.dart
@@ -1,65 +1,87 @@
-
+import 'package:e_commerce_mobile_app/providers/index_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class TopicRow extends StatelessWidget {
+class TopicRow extends StatefulWidget {
   const TopicRow({
     super.key,
   });
 
   @override
+  State<TopicRow> createState() => _TopicRowState();
+}
+
+class _TopicRowState extends State<TopicRow> {
+  @override
   Widget build(BuildContext context) {
-    return const Row(
+    return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <TopicButton>[
-        TopicButton(text: 'Hottest', isActive: true),
-        TopicButton(text: 'Popular', isActive: false),
-        TopicButton(text: 'Newest', isActive: false),
-        TopicButton(text: 'Trending', isActive: false),
+        TopicButton(text: 'Hottest', index: 0, onTap: () => Provider.of<IndexProvider>(context, listen: false).setIndex(0)),
+        TopicButton(text: 'Popular', index: 1, onTap: () => Provider.of<IndexProvider>(context, listen: false).setIndex(1)),
+        TopicButton(text: 'Newest', index: 2, onTap: () => Provider.of<IndexProvider>(context, listen: false).setIndex(2)),
+        TopicButton(text: 'Top', index: 3, onTap: () => Provider.of<IndexProvider>(context, listen: false).setIndex(3)),
       ],
     );
   }
 }
 
-class TopicButton extends StatelessWidget {
+class TopicButton extends StatefulWidget {
   final String text;
-  final bool isActive;
-  final VoidCallback? onPressed;
+  final int index;
+  final VoidCallback? onTap;
   const TopicButton({
     super.key,
     required this.text,
-    this.isActive = false,
-    this.onPressed,
+    required this.index,
+    this.onTap,
   });
 
   @override
+  State<TopicButton> createState() => _TopicButtonState();
+}
+
+class _TopicButtonState extends State<TopicButton> {
+  @override
   Widget build(BuildContext context) {
     return TextButton(
-      onPressed: () {},
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          !isActive
-              ? Text(
-                  text,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(color: Color(0xFF928DB4), fontSize: 16, fontFamily: 'Brandon Grotesque', fontWeight: FontWeight.w500, letterSpacing: -0.16),
-                )
-              : Text(
-                  text,
-                  style: const TextStyle(color: Color(0xFF27214D), fontSize: 24, fontFamily: 'Brandon Grotesque', fontWeight: FontWeight.w500, letterSpacing: -0.24),
-                ),
-          const SizedBox(height: 1),
-          isActive
-              ? Container(
-                  width: 22,
-                  decoration: const ShapeDecoration(
-                    shape: RoundedRectangleBorder(
-                      side: BorderSide(width: 1, strokeAlign: BorderSide.strokeAlignCenter, color: Color(0xFFFFA451)),
+      onPressed: widget.onTap,
+      child: AnimatedDefaultTextStyle(
+        duration: const Duration(milliseconds: 100),
+        style: Provider.of<IndexProvider>(context).index != widget.index
+            ? const TextStyle(
+                color: Color(0xFF928DB4),
+                fontSize: 16,
+                fontFamily: 'Brandon Grotesque',
+                fontWeight: FontWeight.w500,
+                letterSpacing: -0.16,
+              )
+            : const TextStyle(
+                color: Color(0xFF27214D),
+                fontSize: 24,
+                fontFamily: 'Brandon Grotesque',
+                fontWeight: FontWeight.w500,
+                letterSpacing: -0.24,
+              ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 40,
+              child: Text(widget.text),
+            ),
+            Provider.of<IndexProvider>(context).index == widget.index
+                ? Container(
+                    width: 22,
+                    decoration: const ShapeDecoration(
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(width: 1, strokeAlign: BorderSide.strokeAlignCenter, color: Color(0xFFFFA451)),
+                      ),
                     ),
-                  ),
-                )
-              : Container(),
-        ],
+                  )
+                : Container()
+          ],
+        ),
       ),
     );
   }
